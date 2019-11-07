023fab2e7f1a288ded9b5a6419a76f669ae98ca2
Charlie Broom <broomc425@gmail.com>
2019-11-07 01:53:02 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,13 +1,12 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
+      :patriarch => {name: "Lord Montague", age: "53"
+        },
+      :matriarch => {name: "Lady Montague", age: "54"
         #your key/value pairs here
         },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :hero => {
+      :hero => {name: "Romeo", age: "15", status: "alive"
         #your key/value pairs here
         },
       :hero_friends => []
